62e6b78fcd74cd249eff877acf49767746915fb8
Shubham Shinde <shubham.shinde@perforce.com>
2024-07-09 23:39:39 +0530

    (PA-6507) Patch rexml for CVE-2024-35176
     - Ruby 3.2.4 has rexml as its bundled gem, so the oatch was applied after the install step since bundled gems are available in the build after the install step in the .bundle folder.
     - Ruby 2.7.8 has rexml as its default gem, so we can go with the usual way to patch it.
     - The upstream fix commit: https://github.com/ruby/rexml/commit/4325835f92f3f142ebd91a3fdba4e1f1ab7f1cfb
     -

--- configs/components/ruby-3.2.4.rb
+++ configs/components/ruby-3.2.4.rb
@@ -309,17 +309,10 @@ component 'ruby-3.2.4' do |pkg, settings, platform|
   pkg.add_source("file://resources/patches/ruby_32/rexml_for_CVE-2024-35176.patch")
 
   pkg.build do
-    # This patch is applied after install because rexml gem is the bundled gem unlike default gems and hence
-    # not able to find the path of the files to be patched prior configuring and installing
-    #this patch is not required in ruby >= 3.2.5
-    ruby = "#{ruby_bindir}/ruby -rrbconfig"
-    steps = [
-      "export RUBYHDRDIR=$(shell #{ruby} -e 'puts RbConfig::CONFIG[\"rubyhdrdir\"]')",
-      "export VENDORARCHDIR=$(shell #{ruby} -e 'puts RbConfig::CONFIG[\"vendorarchdir\"]')",
-      "export ARCHDIR=$${RUBYHDRDIR}/$(shell #{ruby} -e 'puts RbConfig::CONFIG[\"arch\"]')",
-      "export INCLUDESTR=\"-I#{settings[:includedir]} -I$${RUBYHDRDIR} -I$${ARCHDIR}\""
-    ]
-    steps << ["#{platform.patch} --strip=1 --fuzz=0 --ignore-whitespace --no-backup-if-mismatch < ../rexml_for_CVE-2024-35176.patch"]
+    # This patch is applied after install because rexml gem is the bundled gem hence build
+    # cannot find the path of the files to be patched prior to configuring and installing.
+    # This patch is not required for ruby >= 3.3.3
+    steps = ["#{platform.patch} --strip=1 --fuzz=0 --ignore-whitespace --no-backup-if-mismatch < ../rexml_for_CVE-2024-35176.patch"]
   end
 
 end
